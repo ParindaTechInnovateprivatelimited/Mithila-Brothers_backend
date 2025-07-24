73af84b2485954cc4bf963a87759bceb6a226eae
07Akashh <aonecreations1@gmail.com>
2025-07-25 01:19:39 +0530

    fear

--- config/clodinary.js
+++ config/clodinary.js
@@ -1,9 +1,12 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2,
+config = {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  }
 
-cloudinary.config({
-    cloud_name: 'dmao0koo4',
-    api_key: '694541693673475',
-    api_secret: '63hKcMDGtImv3XxcSkumKLLStao'
-});
+cloudinary.config(config);
+
+console.log('✅ Cloudinary configured successfully');
 
 module.exports = cloudinary;
--- firebase.js
+++ firebase.js
@@ -1,8 +1,22 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./ecomm-241c2-firebase-adminsdk-259nh-3de4db7745.json');
+
+const serviceAccount = {
+    type: 'service_account',
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // VERY IMPORTANT
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURIComponent(process.env.FIREBASE_CLIENT_EMAIL)}`,
+  };
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
+
+console.log('✅ Firebase Admin initialized successfully');
 
 module.exports = admin;
