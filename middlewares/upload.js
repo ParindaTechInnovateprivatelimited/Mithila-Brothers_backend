d66b575c0dd5bb1528cfbbc2e3a99aec416d2f83
07Akashh <aonecreations1@gmail.com>
2025-07-27 00:28:34 +0530

    update uploader

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,19 +1,29 @@
 const cloudinary = require('../config/clodinary');
+const streamifier = require('streamifier');
 
+/**
+ * Upload an in-memory buffer (file.data) to Cloudinary.
+ * @param {object} file - The file object from express-fileupload (in-memory).
+ * @returns {Promise<string>} - The secure URL of the uploaded file.
+ */
 const uploadToCloudinary = (file) => {
     return new Promise((resolve, reject) => {
-        const options = file.tempFilePath;
-        cloudinary.uploader.upload(file.tempFilePath,{
-            folder: 'uploads',
-            use_filename: true
-        }, (error, result) => {
-            if (error) {
-                // console.error('Cloudinary Upload Error:', error);
-                reject(error);
-            } else {
+        const uploadStream = cloudinary.uploader.upload_stream(
+            {
+                folder: 'uploads',
+                use_filename: true,
+                resource_type: 'auto', // supports image, video, etc.
+            },
+            (error, result) => {
+                if (error) {
+                    return reject(error);
+                }
                 resolve(result.secure_url);
             }
-        });
+        );
+
+        // Convert buffer to stream and pipe to Cloudinary
+        streamifier.createReadStream(file.data).pipe(uploadStream);
     });
 };
 
